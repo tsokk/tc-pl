07b87da8abf37371a0a8e1da63a721e05b14054a
tsokk <tso@kapsi.fi>
2021-02-12 16:38:19 +0200

    Remove naked news

--- ch10/x10.cpp
+++ ch10/x10.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <memory>
 
-char * rev(char *s) {
+std::unique_ptr<char[]> rev(char *s) {
   int len = 0;
   for (int i = 0; s[i] != '\0'; ++i)
     ++len;
   const int n = len;
-  char *res = new char[n];
+  std::unique_ptr<char[]> res {new char[n]};
   for (auto i = 0; i < n; ++i) {
     res[i] = s[len-1];
     --len;
@@ -15,5 +16,5 @@ char * rev(char *s) {
 
 int main() {
   char s[] = {"abcdef"};
-  std::cout << rev(s) << '\n';
+  rev(s);
 }
--- ch10/x9.cpp
+++ ch10/x9.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstring>
+#include <memory>
 
-char * cat(char *s1, char *s2) {
+std::unique_ptr<char[]> cat(char *s1, char *s2) {
   const int n1 = strlen(s1);
   const int n2 = strlen(s2);
   const int n = n1 + n2;
-  char *res = new char[n];
+  std::unique_ptr<char[]> res {new char[n]};
   for (auto i = 0; i < n1; ++i)
     res[i] = s1[i];
   for (auto i = n1; i < n1 + n2; ++i)
@@ -16,5 +17,5 @@ char * cat(char *s1, char *s2) {
 int main() {
   char s1[] = {"abc"};
   char s2[] = {"def"};
-  std::cout << cat(s1, s2) << '\n';
+  cat(s1, s2);
 }
