41be877e0cdcc088c7d1d9c77a3b1557ff094973
tsokk <tso@kapsi.fi>
2021-02-23 14:23:39 +0200

    Add chapter 12 solution 2

---
+++ ch12/x2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Define an alias for a reference to a function taking two integer parameters,
+// and returning an integer. Useful, if one needs to pass such function as
+// argument to another function.
+using riffi = int (&)(int, int);
+
+int main() { }
+
