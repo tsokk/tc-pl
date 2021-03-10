b009aa5821933bcd9fa2e6c39be4607d3d05a8ec
tsokk <tso@kapsi.fi>
2021-03-10 11:22:31 +0200

    Change key, use wide characters

--- ch12/encrypt.cpp
+++ ch12/encrypt.cpp
@@ -4,19 +4,24 @@
 void cipher(std::string &message, std::string &key) {
   int klen = key.size();
   int mlen = message.size();
-  for (int i = 0; i < mlen; ++i)
-    std::cout << char(message[i] ^ key[i % klen]);
+  for (int i = 0; i < mlen; ++i) {
+    wchar_t m = message[i];
+    wchar_t k = key[i % klen];
+    wchar_t e = m^k;
+    std::wcout << e;
+  }
+  std::cout << '\n';
 }
 
 void ask_for(std::string &s, std::string msg) {
-	std::cout << msg << '\n';
+  std::cout << msg << '\n';
   getline(std::cin, s);
 }
 
 int main() {
-  std::string plaintext;
-	std::string key;
-	ask_for(plaintext, "Enter plaintext:");
-	ask_for(key, "Enter key:");
-	cipher(plaintext, key);
+  std::string msg;
+  std::string key;
+  ask_for(msg, "Enter message:");
+  ask_for(key, "Enter key:");
+  cipher(msg, key);
 }
